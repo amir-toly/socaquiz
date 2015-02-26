7d85cca52e4988dd3799733f2d04b9b7a6534b06
Amir TOLY <amir.toly@gmail.com>
2015-02-26 09:33:21 +0100

    Return HelloWorld POJO as JSON

--- src/com/socaquiz/HelloWorldController.java
+++ src/com/socaquiz/HelloWorldController.java
@@ -3,18 +3,18 @@ package com.socaquiz;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloWorldController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView printHello() {
+	public @ResponseBody HelloWorld printHello() {
 		
 		HelloWorld helloWorld = new HelloWorld();
 		helloWorld.setName("Spring MVC Framework!");
 		
-		return new ModelAndView("hello", "helloWorld", helloWorld);
+		return helloWorld;
 	}
 }
