2056e49fd35c72afc02e0eb4b1f838a559b73af1
Amir TOLY <amir.toly@gmail.com>
2015-03-01 12:04:07 +0100

    Get POJO from database using JPA + Hibernate

--- src/com/socaquiz/HelloWorld.java
+++ src/com/socaquiz/HelloWorld.java
@@ -1,8 +1,29 @@
 package com.socaquiz;
 
-public class HelloWorld {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "HELLOWORLDS")
+public class HelloWorld {
+	
+	private Long id;
 	private String name;
+
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
--- src/com/socaquiz/HelloWorldController.java
+++ src/com/socaquiz/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.socaquiz;
 
+import javax.persistence.Persistence;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,10 +13,14 @@ public class HelloWorldController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public @ResponseBody HelloWorld printHello() {
+		HelloWorldDao helloWorldDao = new HelloWorldDao(
+				Persistence.createEntityManagerFactory("com.socaquiz.jpa"));
 		
 		HelloWorld helloWorld = new HelloWorld();
 		helloWorld.setName("Spring MVC Framework!");
 		
-		return helloWorld;
+		helloWorldDao.create(helloWorld);
+		
+		return helloWorldDao.getById(helloWorld.getId());
 	}
 }
